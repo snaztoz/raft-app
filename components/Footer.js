e239d593f4fa57d6c84b1cdd2d2248080c349550
snaztoz <hafidhmn@gmail.com>
2021-04-04 22:10:40 +0700

    Membuat komponen Footer.

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="p-2">
+      <div className="p-2 border-t border-gray-200 text-center">
+        <p>Raft</p>
+        <p>Copyright &copy; 2021 Raft. All Rights Reserved.</p>
+      </div>
+    </footer>
+  )
+}
