7d1c886486bda60d2d3717554b2009271a153ba7
snaztoz <hafidhmn@gmail.com>
2021-04-08 11:26:42 +0700

    Membuat input field dapat menampilkan error.

--- components/Forms.js
+++ components/Forms.js
@@ -4,11 +4,25 @@ const Input = props => {
   const inputType = props.inputType
   const name = props.name
   const placeholder = props.placeholder
+  const isErrorExist = props.isErrorExist
+  const errorMsg = props.errorMsg
 
   return (
-    <input type={inputType} name={name} placeholder={placeholder}
-        className={`${style.no_outline} w-full p-3 rounded border-b-2
-        border-gray-300 focus:border-green-500`}/>
+    <>
+      <input type={inputType} name={name} placeholder={placeholder}
+          className={`
+            ${style.no_outline}
+            ${isErrorExist? 'border-red-400': 'border-gray-300 focus:border-green-500'}
+            w-full px-3 py-2 rounded border-b-2`}/>
+
+      <p className="text-left px-3">
+        <small className={`
+            text-red-400
+            ${isErrorExist? null: 'invisible'}`}>
+          {errorMsg? errorMsg: 'no error'}
+        </small>
+      </p>
+    </>
   )
 }
 
