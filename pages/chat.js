78a5fddca74615de688aa652c4f2c4d09a56e2b3
snaztoz <hafidhmn@gmail.com>
2021-04-13 21:34:41 +0700

    Membuat tampilan untuk typing area.

--- pages/chat.js
+++ pages/chat.js
@@ -4,6 +4,7 @@ import { withIronSession } from 'next-iron-session'
 import Container from 'components/Container'
 import session from 'values/session'
 import url from 'values/urls'
+import { ActionButton } from 'components/Button'
 import { ProfileHeader } from 'components/chat/ProfileHeader'
 
 export const getServerSideProps = withIronSession(
@@ -37,9 +38,24 @@ export default function Chat({ user }) {
           <ProfileHeader bgColor="bg-green-300" textColor="text-white" name="Foo"
               image="/me-square.jpg" otherActions="act" />
         </aside>
-        <main className="col-span-8 h-full bg-red-100">
+        <main className="col-span-8 h-full bg-red-100 flex flex-col justify-between">
           <ProfileHeader bgColor="bg-white" textColor="text-dark" name="Bar"
-             image="/me-square.jpg" otherActions="act" />
+              image="/me-square.jpg" otherActions="act" />
+
+          <div className="h-full">
+            <h1>MessagesArea</h1>
+          </div>
+
+          <div className="bg-white flex px-3 py-2">
+            <textarea className="mr-3 py-3 px-5 w-full resize-none outline-none bg-gray-200
+                rounded-2xl" rows="1"></textarea>
+
+            <div className="flex items-center">
+              <ActionButton buttonType="primary" text="Send"
+                  style="p-0"
+                  handleClick={() => console.log('CLICKED')}/>
+            </div>
+          </div>
         </main>
       </div>
     </Container>
