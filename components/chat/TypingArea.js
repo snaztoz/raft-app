849fa689cacf8be799dd2f51a3606d7f7704598a
snaztoz <hafidhmn@gmail.com>
2021-04-13 22:13:16 +0700

    Menambahkan placeholder di typing box.

--- components/chat/TypingArea.js
+++ components/chat/TypingArea.js
@@ -4,7 +4,7 @@ export const TypingArea = () => {
   return (
     <div className="bg-white flex px-3 py-2">
       <textarea className="w-full ml-1 mr-3 py-3 px-5 resize-none outline-none
-          bg-gray-200 rounded-2xl" rows="1"></textarea>
+          bg-gray-200 rounded-2xl" rows="1" placeholder="Type something"></textarea>
 
       <div className="mr-3 flex items-center">
         <ActionButton buttonType="primary" text="Send"
