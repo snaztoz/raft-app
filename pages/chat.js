9d895fb759e264fff40589737f6ad0cc8f7ee33c
snaztoz <hafidhmn@gmail.com>
2021-04-13 22:02:04 +0700

    Fix beberapa posisi layout.

--- pages/chat.js
+++ pages/chat.js
@@ -55,12 +55,11 @@ export default function Chat({ user }) {
           </div>
 
           <div className="bg-white flex px-3 py-2">
-            <textarea className="mr-3 py-3 px-5 w-full resize-none outline-none bg-gray-200
-                rounded-2xl" rows="1"></textarea>
+            <textarea className="w-full ml-1 mr-3 py-3 px-5 resize-none outline-none
+                bg-gray-200 rounded-2xl" rows="1"></textarea>
 
-            <div className="flex items-center">
+            <div className="mr-3 flex items-center">
               <ActionButton buttonType="primary" text="Send"
-                  style="p-0"
                   handleClick={() => console.log('CLICKED')}/>
             </div>
           </div>
